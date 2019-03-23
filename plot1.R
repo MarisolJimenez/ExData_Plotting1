a901382e0aa29701c84d7480b142144bd4331bda
MarisolJimenez <48668041+MarisolJimenez@users.noreply.github.com>
2019-03-22 20:55:13 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,21 @@
+df<-read.table("household_power_consumption.txt", header=FALSE, sep=";",skip=66637,
+               nrow = 2880,col.names = colnames(read.table("household_power_consumption.txt", 
+               header=TRUE, sep=";",nrow=1)))
+    
+attach(df)
+df$Time<-strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+df$Date<-as.Date(df$Date,"%d/%m/%Y")
+#To know what were the right numbers for "skip" and "nrow" at first I wrote 1,4 and see that 
+#every row was a minute so I just made the math to know how many rows per day I need to 
+#substract 
+library(gdata) #to use the command unknownToNA to change the ? values into NA 
+df<-unknownToNA(x=df,unknown="?")
+df<-na.omit(df)
+     
+       ##PLOT 
+hist(Global_active_power,col="red",
+ xlab="Global Active Power (kilowatts)",ylab = "Frequency",
+ main="Global Active Power")
+
+dev.copy(png,file="plot1.png")
+
---
+++ plot3.R
@@ -0,0 +1,25 @@
+df<-read.table("household_power_consumption.txt", header=FALSE, sep=";",skip=66637,
+               nrow = 2880,col.names = colnames(read.table("household_power_consumption.txt", 
+                                                           header=TRUE, sep=";",nrow=1)))
+
+attach(df)
+df$Time<-strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+df$Date<-as.Date(df$Date,"%d/%m/%Y")
+#To know what were the right numbers for "skip" and "nrow" at first I wrote 1,4 and see that 
+#every row was a minute so I just made the math to know how many rows per day I need to 
+#substract 
+library(gdata) #to use the command unknownToNA to change the ? values into NA 
+df<-unknownToNA(x=df,unknown="?")
+df<-na.omit(df)
+
+##PLOT 3
+with(df,plot(Time,Sub_metering_1,type="n", 
+             ylab="Every sub metering",xlab = "")) 
+#Sub meterings
+with(df,points(Time,Sub_metering_1,type="l", col="black"))
+with(df,points(Time,Sub_metering_2,type="l", col="red"))
+with(df,points(Time,Sub_metering_3,type="l", col="blue"))
+legend("topright",lty=1,cex=0.8 ,col=c("black","red","blue"),
+       legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+
+dev.copy(png,file="plot3.png")
